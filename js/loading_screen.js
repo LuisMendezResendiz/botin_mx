dc0eb5b7c54232f471c01ee13150b25530722869
Luis Méndez <luismendez220796@gmail.com>
2018-11-27 00:16:13 -0600

    Se arreglaron errores en las pantallas de carga e imágenes

--- js/loading_screen.js
+++ js/loading_screen.js
@@ -1,3 +1,7 @@
 window.addEventListener("load", function() {
 	setTimeout('document.getElementById("loading_screen").style.visibility = "hidden"', 1700);
 });
+
+window.addEventListener("load", function() {
+	setTimeout('document.getElementById("loading_screen_02").style.visibility = "hidden"', 1000);
+});
