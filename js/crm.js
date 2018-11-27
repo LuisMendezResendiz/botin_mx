16dc2ba4fdb2d082882b5249ab0c678db2c9531b
GarooEBM <ebalderas03@alumnos.uaq.mx>
2018-11-26 22:13:06 -0600

    Graficas culeras

---
+++ js/crm.js
@@ -0,0 +1,77 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawVisualization);
+
+function drawVisualization() {
+
+	var est = new Array();//Arreglo para estados
+	est.push('Estado')
+	var numEst = new Array();
+	numEst.push('');
+
+	var paq = new Array();//Arreglo para tipos
+	paq.push('Tipo de Paquete')
+	var numPaq = new Array();
+	numPaq.push('');
+
+	//SE LLENAN LOS ARREGLOS
+	db.collection("estados").get().then(function(querySnapshot) {
+	    querySnapshot.forEach(function(doc) {
+	        est.push(doc.id);
+	        numEst.push(doc.data().conteo);
+	    });
+
+	    console.log(est);
+	    console.log(numEst);
+	    graf(est, numEst);
+	});
+	db.collection("paquetes").get().then(function(querySnapshot) {
+	    querySnapshot.forEach(function(doc) {
+	        paq.push(doc.id);
+	        numPaq.push(doc.data().numero);
+	    });
+	    console.log(paq);
+	    console.log(numPaq);
+	    graf2(paq, numPaq);
+	});
+}
+
+function graf(est, numEst){
+	var data = google.visualization.arrayToDataTable([
+		[est[0], est[1], est[2], est[3]],
+		[numEst[0], numEst[1], numEst[2], numEst[3]]
+	]);
+
+
+	var options = {
+	title : 'Compras por estado',
+	vAxis: {title: 'Compras'},
+	hAxis: {title: 'Estado'},
+	seriesType: 'bars',
+	series: {5: {type: 'line'}},
+	colors: ['#05C7FF', '#479DE4', '#8874C8', '#D842AE', '#f3b49f']
+	};
+
+	var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+}
+
+function graf2(paq, numPaq){
+	var data2 = google.visualization.arrayToDataTable([
+		[paq[0], paq[1], paq[2], paq[3]],
+		[numPaq[0], numPaq[1], numPaq[2], numPaq[3]]
+	]);
+
+
+	var options2 = {
+	title : 'Compras por paquete',
+	vAxis: {title: 'Compras'},
+	hAxis: {title: 'Paquete'},
+	seriesType: 'bars',
+	series: {5: {type: 'line'}},
+	colors: ['#05C7FF', '#479DE4', '#8874C8', '#D842AE', '#f3b49f']
+	};
+
+	var chart = new google.visualization.ComboChart(document.getElementById('chart_div2'));
+	chart.draw(data2, options2);
+}
+
